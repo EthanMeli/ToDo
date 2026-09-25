0db6d75d8b06142946fb66ae1de0a2efdc012b20
Ethan Meli <etme6835@colorado.edu>
2026-09-25 11:03:44 -0600

    Fix webpack configuration

--- webpack.common.js
+++ webpack.common.js
@@ -1,28 +1,12 @@
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-  entry: {
-    app: './src/index.js',
-  },
-  experiments: {
-    html: true,
-  },
+  entry: "./src/index.js",
   output: {
-    filename: '[name].bundle.js',
-    htmlFilename: 'index.html',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "index_bundle.js",
+    path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
-    html: {
-      meta: {
-        charset: 'UTF-8',
-        viewport: 'width=device-width, initial-scale=1',
-      },
-      title: 'Production',
-    },
   },
   plugins: [
     new HtmlWebpackPlugin({
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,7 @@ import { merge } from 'webpack-merge';
 import common from './webpack.common.js';
 
 export default merge(common, {
-  mode: 'development',
+  mode: "development",
   devtool: "eval-source-map",
   devServer: {
     watchFiles: ["./src/index.html"],
